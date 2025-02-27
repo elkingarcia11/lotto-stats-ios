8562b8a53d4effb7da718dfecb0bfd4948eb7bbf
Elkin Garcia <elkingarcia.11@gmail.com>
2025-02-27 15:38:55 -0500

    Initial Commit

---
+++ Lotto Stats/Lotto_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lotto_StatsApp.swift
+//  Lotto Stats
+//
+//  Created by Elkin Garcia on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Lotto_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
